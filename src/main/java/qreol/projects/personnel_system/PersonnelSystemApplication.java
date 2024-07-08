96c5d8d14b0b36144673922be11c8311d0af8eaa
qreol <stim19255@gmail.com>
2024-07-08 19:08:45 +0700

    init

---
+++ src/main/java/qreol/projects/personnel_system/PersonnelSystemApplication.java
@@ -0,0 +1,13 @@
+package qreol.projects.personnel_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonnelSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonnelSystemApplication.class, args);
+	}
+
+}
